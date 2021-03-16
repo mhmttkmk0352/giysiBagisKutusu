50a8a070431727690f93862b2d30f80466950d7a
mhmttkmk0352 <mehmet.tkmk.0352@gmail.com>
2021-03-17 00:04:41 +0300

    .

--- pages/anasayfa.js
+++ pages/anasayfa.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+console.log( AsyncStorage );
 
 class App extends React.Component{
   render(){
